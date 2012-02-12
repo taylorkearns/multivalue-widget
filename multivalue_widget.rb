b467426cb2cc6b2714c71a77af13c7aa8d025c81
Taylor Kearns <tkearns@gmail.com>
2012-02-12 12:42:03 -0500

    Updated routes/controllers and added more test data.

--- multivalue_widget.rb
+++ multivalue_widget.rb
@@ -1,25 +1,25 @@
 require 'sinatra'
 require 'data_mapper'
 
+# ==================================================
 # Routes
+# ==================================================
 get '/' do
-
 	@user = User.first
-	@skills = Skill.all
-	
-	@skills = @skills.count > 0 ? order_and_organize(@skills) : @skills
-	
+	@skills = Skill.all(:order => [:name])
+	tree_builder = TreeBuilder.new
+	@skills = @skills.count > 0 ? tree_builder.order_and_organize(@skills) : @skills
+	@user_skills = @user.all_skills
 	erb :index
-	
 end
 
 put '/' do
-
 	erb :index
-
 end
 
+# ==================================================
 # Models
+# ==================================================
 class Skill
 	include DataMapper::Resource
 	
@@ -38,58 +38,81 @@ class User
 	property :first_name, String, :required => true
 	
 	has n, :skills, :through => Resource
-end
-
-def children?(skill_id)
-	Skill.all(:parent_id => skill_id).count > 0
-end
-
-def build_children(skill)
-	html = '<ul>'
 	
-	children = Skill.all(:parent_id => skill[:id])
-	children.each do |child|
-		html += '<li>'+child[:name]
-		if children?(child[:id])
-			html += '<img class="open arrow" src="images/open.png"/>'
-			build_children(child)
-		end
+	def all_skills
+		skills.all(:order => [:name])
+	end
+end
+
+# ==================================================
+# Tree builder functions
+# ==================================================
+class TreeBuilder
+	def children?(id)
+		Skill.all(:parent_id => id).count > 0
 	end
 	
-	html += '</ul>'			
-	html	
-end
-
-def order_and_organize(skills)
-	html = '<ul>'
-	
-	# Get all of the root level skills
-	skills.each do |skill|
-		if skill[:parent_id] === nil
-			html += '<li>'+skill[:name]
-			
-			if children?(skill[:id])
-				html += '<img class="open arrow" src="images/open.png"/>'
-				build_children(skill)
+	def build_children(skill)
+		child_html = '<ul>'
+		
+		# Recursively traverse through child skills and build lists within lists
+		children = Skill.all(:parent_id => skill[:id])
+		children.each do |child|
+			child_html += '<li>'+child[:name]
+			if children?(child[:id])
+				child_html += '<img class="open arrow" src="images/open.png"/>'
+				child_html += build_children(child)
+			else
+				child_html += '<img class="add" src="images/add.png"/>'
 			end
 			
-			html += '</li>'
+			child_html += '</li>'
 		end
-	end
 		
-	html += '</ul>'	
-	html
+		child_html += '</ul>'			
+		child_html	
+	end
+	
+	def order_and_organize(skills)
+		html = '<ul>'
+		
+		# Get root level skills
+		skills.each do |skill|
+			if skill[:parent_id] === nil
+				html += '<li>'+skill[:name]
+				
+				if children?(skill[:id])
+					html += '<img class="open arrow" src="images/open.png"/>'
+					html += build_children(skill)
+				else
+					html += '<img class="add" src="images/add.png"/>'
+				end
+				
+				html += '</li>'
+			end
+		end
+			
+		html += '</ul>'	
+		html
+	end
 end
 
+# ==================================================
 # Datamapper setup
+# ==================================================
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, 'sqlite:///Users/tkearns/_projects/TK/ruby/multivalue-widget/multivalue-widget/multivalue_widget.db')
 DataMapper.finalize
 DataMapper.auto_migrate!
 #DataMapper.auto_upgrade!
 
+
+# ==================================================
+# Test data
+# ==================================================
 @u = User.create(:first_name => 'Taylor', :last_name => 'Kearns')
 p @u
+
 @s1 = Skill.create(:name => 'Programming')
 p @s1
 @s2 = Skill.create(:name => 'Ruby', :parent_id => 1)
@@ -99,10 +122,35 @@ p @s3
 @s4 = Skill.create(:name => 'Python', :parent_id => 1)
 p @s4
 @s11 = Skill.create(:name => 'Exercise')
-p @s11[:parent_id]
-@s12 = Skill.create(:name => 'Squats', :parent_id => 5)
+p @s11
+@s12 = Skill.create(:name => 'Weight Lifting', :parent_id => 5)
 p @s12
-@s13 = Skill.create(:name => 'Dead Lifts', :parent_id => 5)
+@s121 = Skill.create(:name => 'Squats', :parent_id => 6)
+p @s121
+@s122 = Skill.create(:name => 'Dead Lifts', :parent_id => 6)
+p @s122
+@s123 = Skill.create(:name => 'Bench Press', :parent_id => 6)
+p @s123
+@s13 = Skill.create(:name => 'Cardio', :parent_id => 5)
 p @s13
-@s14 = Skill.create(:name => 'Bench Press', :parent_id => 5)
-p @s14[:parent_id]
+@s14 = Skill.create(:name => 'Agility', :parent_id => 5)
+p @s14
+@s15 = Skill.create(:name => 'Pottery')
+p @s15
+@s16 = Skill.create(:name => 'Crochet')
+p @s16
+@s17 = Skill.create(:name => 'Telepathy')
+p @s17
+
+@u.skills << @s2
+@u.save
+
+@u.skills.each { |user_skill| puts "USER SKILL >> #{ user_skill[:name] }" }
+
+
+
+
+
+
+
+
